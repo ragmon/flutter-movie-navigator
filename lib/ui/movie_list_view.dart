e6f45072561b02ad169a687e40e817e351a72152
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-06 14:55:17 +0300

    update

--- lib/ui/movie_list_view.dart
+++ lib/ui/movie_list_view.dart
@@ -16,7 +16,13 @@ class MovieListView extends StatelessWidget {
       body: ListView.builder(
         itemCount: movieList.length,
         itemBuilder: (BuildContext context, int index) {
-          return movieCard(movieList[index], context);
+          return Stack(children: [
+            movieCard(movieList[index], context),
+            Positioned(
+              top: 10.0,
+              child: movieImage(movieList[index].images[0]),
+            ),
+          ]);
 //          return Card(
 //            elevation: 4.5,
 //            color: Colors.white,
@@ -58,37 +64,93 @@ class MovieListView extends StatelessWidget {
   Widget movieCard(Movie movie, BuildContext context) {
     return InkWell(
       child: Container(
+        margin: EdgeInsets.only(left: 60.0),
         width: MediaQuery.of(context).size.width,
         height: 120.0,
         child: Card(
           color: Colors.black45,
           child: Padding(
             padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 54.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(movie.title),
-                    Text("Rating: ${movie.imdbRating} / 10"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Text("Relesed: ${movie.released}"),
-                    Text(movie.runtime),
-                    Text(movie.rated),
-                  ],
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        child: Text(
+                          movie.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Rating: ${movie.imdbRating} / 10",
+                        style: mainTextStyle(),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Relesed: ${movie.released}",
+                        style: mainTextStyle(),
+                      ),
+                      Text(
+                        movie.runtime,
+                        style: mainTextStyle(),
+                      ),
+                      Text(
+                        movie.rated,
+                        style: mainTextStyle(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-      onTap: () => debugPrint(movie.title),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MovieListViewDetails(
+              movie: movie,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  TextStyle mainTextStyle() {
+    return TextStyle(
+      fontSize: 15.0,
+      color: Colors.grey,
+    );
+  }
+
+  Widget movieImage(String imageUrl) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: NetworkImage(imageUrl ??
+              "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
