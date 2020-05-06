eda9d84d981ef1096f3ef21caa6b4e96942bae20
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-06 15:02:28 +0300

    changed background color

--- lib/ui/movie_list_view.dart
+++ lib/ui/movie_list_view.dart
@@ -12,7 +12,7 @@ class MovieListView extends StatelessWidget {
         title: Text("Movies"),
         backgroundColor: Colors.blueGrey.shade900,
       ),
-      backgroundColor: Colors.blueGrey.shade400,
+      backgroundColor: Colors.blueGrey.shade900,
       body: ListView.builder(
         itemCount: movieList.length,
         itemBuilder: (BuildContext context, int index) {
@@ -23,39 +23,6 @@ class MovieListView extends StatelessWidget {
               child: movieImage(movieList[index].images[0]),
             ),
           ]);
-//          return Card(
-//            elevation: 4.5,
-//            color: Colors.white,
-//            child: ListTile(
-//              leading: CircleAvatar(
-//                child: Container(
-//                  width: 200,
-//                  height: 200,
-//                  decoration: BoxDecoration(
-//                    image: DecorationImage(
-//                      image: NetworkImage(movieList[index].images[0]),
-//                      fit: BoxFit.cover,
-//                    ),
-//                    borderRadius: BorderRadius.circular(13.9),
-//                  ),
-//                  child: null,//Text(movieList[index].title[0]),
-//                ),
-//              ),
-//              trailing: Text("..."),
-//              title: Text(movieList[index].title),
-//              subtitle: Text(movieList[index].actors),
-//              onTap: () => {
-//                Navigator.push(
-//                  context,
-//                  MaterialPageRoute(
-//                    builder: (context) => MovieListViewDetails(
-//                      movie: movieList[index],
-//                    ),
-//                  ),
-//                ),
-//              },
-//            ),
-//          );
         },
       ),
     );
