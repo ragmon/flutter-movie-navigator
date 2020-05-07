defc196e4d5b6d665c7621b8d3a4ce1e0348392e
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-07 12:03:44 +0300

    added MovieDetailsCast

--- lib/ui/movie_list_view_details.dart
+++ lib/ui/movie_list_view_details.dart
@@ -17,6 +17,7 @@ class MovieListViewDetails extends StatelessWidget {
         children: <Widget>[
           MovieDetailsThumbnail(thumbnail: movie.images[0]),
           MovieDetailsHeaderWithPoster(movie: movie),
+          MovieDetailsCast(movie: movie),
         ],
       ),
 //      body: Center(
@@ -120,6 +121,60 @@ class MovieDetailsHeader extends StatelessWidget {
             ],
           ),
         )
+      ],
+    );
+  }
+}
+
+class MovieDetailsCast extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsCast({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: <Widget>[
+          MovieField(field: "Cast", value: movie.actors),
+          MovieField(field: "Directors", value: movie.director),
+          MovieField(field: "Awards", value: movie.awards),
+        ],
+      ),
+    );
+  }
+}
+
+class MovieField extends StatelessWidget {
+  final String field;
+  final String value;
+
+  const MovieField({Key key, this.field, this.value}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "$field : ",
+          style: TextStyle(
+            color: Colors.black38,
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 12,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
       ],
     );
   }
